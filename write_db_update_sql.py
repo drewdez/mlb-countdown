9d1b59c154da899288ea6a9556efa83914c555ac
Andrew Desautels <drewdez@gmail.com>
2018-02-02 01:31:02 -0700

    update for 2018 season

--- write_db_update_sql.py
+++ write_db_update_sql.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from bs4 import BeautifulSoup, SoupStrainer
 from selenium import webdriver
 
-CALENDAR_CONTENT = SoupStrainer('div', {'class': 'calendar'})
+CALENDAR_CONTENT = SoupStrainer('div', {'class': 'list-mode'})
+TZ_CONTENT = SoupStrainer('div', {'class': 'timezone-note above-legend'})
 TZ_DICT = {
     'PT': pytz.timezone('US/Pacific'),
     'MST': pytz.timezone('US/Arizona'),
@@ -25,30 +26,29 @@ def main():
     for team in teams:
         month = 2
         while month < 5 and (not 'pc_date' in team or not 'ex_date' in team or not 'rs_date' in team):
-            sched_url = 'http://mlb.com/schedule/index.jsp?c_id={}#y=2017&m={}&calendar=DEFAULT'.format(team['sched_id'], month)
+            sched_url = 'https://www.mlb.com/{}/schedule/{}-0{}/list'.format(team['sched_id'], datetime.now().strftime('%Y'), month)
             driver.get(sched_url)
-            time.sleep(20)
+            time.sleep(30)
             sched_html = driver.page_source
             soup = BeautifulSoup(sched_html, 'html.parser', parse_only = CALENDAR_CONTENT)
-            tz_element = soup.find(class_='calendar-notice')
+            tz_element = BeautifulSoup(sched_html, 'html.parser', parse_only = TZ_CONTENT)
             if not 'timezone' in team:
-                team['timezone'] = get_tz_from_calendar_notice(tz_element)
+                team['timezone'] = get_tz_from_timezone_note(tz_element, team['id'])
                 print team['id'], 'time zone:', team['timezone'].zone
             if not 'pc_date' in team:
-                pc_element = soup.find(text=re.compile("[Pp]itchers and [Cc]atchers"))
+                pc_element = soup.find(class_='non-game-event-title', text=re.compile("[Pp]itchers and [Cc]atchers"))
                 if pc_element:
-                    team['pc_date'] = get_datetime_from_non_game_event(pc_element,
-                        TZ_DICT[team['st_league']])
+                    team['pc_date'] = get_datetime_from_non_game_event(pc_element.parent.parent.parent, TZ_DICT[team['st_league']])
                     print team['id'], 'pitchers and catchers report:', team['pc_date']
             if not 'ex_date' in team:
-                ex_game = soup.find(class_='typeS')
-                if ex_game:
-                    team['ex_date'] = get_datetime_from_game(ex_game, team['timezone'])
+                ex_element = soup.find(class_='game-type-note', text='Spring Training')
+                if ex_element:
+                    team['ex_date'] = get_datetime_from_game(ex_element.parent.parent, team['timezone'])
                     print team['id'], 'exhibition opener:', team['ex_date']
             if not 'rs_date' in team:
-                rs_game = soup.find(class_='typeR')
-                if rs_game:
-                    team['rs_date'] = get_datetime_from_game(rs_game, team['timezone'])
+                rs_element = soup.find(class_='game-type-note', text='Reg. season')
+                if rs_element:
+                    team['rs_date'] = get_datetime_from_game(rs_element.parent.parent, team['timezone'])
                     print team['id'], 'regular season opener:', team['rs_date']
             month += 1
         write_sql_line(output_file, team)
@@ -82,21 +82,20 @@ def write_sql_line(output_file, team):
     with open(output_file, 'a') as output_file:
         output_file.write(team_string)
 
-def get_tz_from_calendar_notice(cal_notice):
-    tz_string = re.match(r"^All [Tt]imes (\w+).",
-        cal_notice.get_text(strip=True)).group(1)
+def get_tz_from_timezone_note(tz_note, team_id):
+    tz_string = re.match(r"^All [Tt]imes (\w+) unless otherwise noted.", tz_note.get_text(strip=True)).group(1)
+    if team_id == 'ARI': tz_string = 'MST'
     return TZ_DICT[tz_string]
 
 def get_datetime_from_game(game, tz):
-    date_string = re.match(r"\d+_(\d{4}-\d{2}-\d{2})", game['data-id']).group(1)
-    time_string = re.match(r"^(\d+:\d{2} \wm)", game.find(class_='game-statusS').get_text(strip=True)).group(1).upper()
-    local = datetime.strptime(date_string + " " + time_string, '%Y-%m-%d %I:%M %p')
+    date_string = re.match(r"(\w{3} \d{2})", game.find(class_='month-date').get_text()).group(1)
+    time_string = re.match(r"^(\d+:\d{2} \wm) (\w{3})", game.find(class_='primary-time').get_text(strip=True)).group(1).upper()
+    local = datetime.strptime(date_string + ' ' + datetime.now().strftime('%Y') + ' ' + time_string, '%b %d %Y %I:%M %p')
     return tz.localize(local).astimezone(pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
 
 def get_datetime_from_non_game_event(event, tz):
-    date_string = re.match(r"date-(\d{4}-\d{2}-\d{2})",
-        event.parent.parent.parent.parent.parent['id']).group(1)
-    local = datetime.strptime(date_string + " 08:00 AM", '%Y-%m-%d %I:%M %p')
+    date_string = re.match(r"(\w{3} \d{2})", event.find(class_='month-date').get_text()).group(1)
+    local = datetime.strptime(date_string + ' ' + datetime.now().strftime('%Y') + ' 08:00 AM', '%b %d %Y %I:%M %p')
     return tz.localize(local).astimezone(pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
 
 if __name__ == '__main__':
